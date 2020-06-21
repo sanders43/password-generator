eaf29cdde0f704f81d886556112f18f68355eb05
Waves <64664814+sanders43@users.noreply.github.com>
2020-06-21 14:27:19 -0500

    variables and password lnegth functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,45 @@
 // Assignment code here
+// Password Characters
+let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+let alphabetCap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let lowercase = alphabet.split("");
+let uppercase = alphabetCap.split("");
+let numbers = '0123456789'
+let specialChars = "!$%&'()*+,-./:;<=>?@[]^_`{|}~"
+console.log(specialChars);
 
 // generate password function
-const generatePassword = () =>{
-  
+const getPasswordLength = () => {
+  let passwordLength = window.prompt("How long would you like your password to be? Please choose a number between 8 and 128.");
+    if(passwordLength >= 8 && passwordLength <= 128) {
+      passwordLength = passwordLength
+      console.log(passwordLength)
+    } else {
+      window.alert("Invalid entry please choose a number between 8 and 128! ");
+      getPasswordLength();
+    }
 };
 
+const getCharTypes = () => {
+  let charLowerCase = window.prompt("Would you like your password to contain lowercase characters? Please choose yes or no.").toLowerCase();
+  if (charLowerCase === "yes") {charLowerCase = lowercase}
+  else if (charLowerCase === "no") {charLowerCase = ""}
+  else {
+    window.alert("Please choose yes or no!");
+    getCharTypes();
+  } 
+  let charUpperCase = window.prompt("Would you like your password to contain uppercase characters? Please choose yes or no.").toLowerCase();
+  if (charUpperCase === "yes") {charUpperCase = uppercase}
+  else if (charUpperCase === "no") {charUpperCase = ""}
+  else {
+    window.alert("Please choose yes or no!");
+    getCharTypes();
+  }
+  console.log(charLowerCase);
+  console.log(charUpperCase);
+};
+getCharTypes();
+ 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
