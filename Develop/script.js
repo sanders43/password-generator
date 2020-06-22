edea56aed4efa17cb636ebe9c45773b5b18487e4
Waves <64664814+sanders43@users.noreply.github.com>
2020-06-22 00:51:28 -0500

    fixed issue with functions not resetting vales when called within themselves

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,8 @@ let alphabetCap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 let numbers = '0123456789'
 let specialChars = "!$%&'()*+,-./:;<=>?@[]^_`{|}~"
 let password = "";
-// Gnerate Password Function Start
+
+// Generate Password Function Start
 
 const generatePassword = () => {
 
@@ -13,89 +14,91 @@ const generatePassword = () => {
 // get password length
 const getPasswordLength = () => {
   let passwordLength = window.prompt("How long would you like your password to be? Please choose a number between 8 and 128.");
+  
     if(passwordLength >= 8 && passwordLength <= 128) {
 
       //creates empty array of length of prompt window input
       passwordLength = Array.from({length:passwordLength})
+      newPasswordLength = passwordLength;
     
     
     } else {
       window.alert("Invalid entry please choose a number between 8 and 128! ");
       getPasswordLength();
     }
-    return passwordLength;
+    return newPasswordLength;
 }
 
-let passwordLength = getPasswordLength();
+ newPasswordLength = getPasswordLength();
 
 
 // choose any or some of lowercase, uppercase, numers and special characters
-const getCharTypes = () => {
+let getCharTypes = () => {
 
 // lowercase
   let charLowerCase = window.prompt("Would you like your password to contain lowercase characters? Please type yes or no.").toLowerCase();
   if (charLowerCase === "yes") {charLowerCase = alphabet}
-  else if (charLowerCase === "no") {charLowerCase = ""}
+  else if (charLowerCase === "no") {charLowerCase = null}
   else {
     window.alert("Please choose yes or no!");
-    getCharTypes();
+    return getCharTypes();
   } 
 
 //uppercase
   let charUpperCase = window.prompt("Would you like your password to contain uppercase characters? Please type yes or no.").toLowerCase();
   if (charUpperCase === "yes") {charUpperCase = alphabetCap}
-  else if (charUpperCase === "no") {charUpperCase = ""}
+  else if (charUpperCase === "no") {charUpperCase = null}
   else {
     window.alert("Please choose yes or no!");
-    getCharTypes();
+    return getCharTypes();
   }
 
 //numbers
   let charNumbers = window.prompt("Would you like your password to contain numbers? Please type yes or no.").toLowerCase();
   if (charNumbers === 'yes') {charNumbers = numbers}
-  else if (charNumbers === 'no') {charNumbers = ""}
+  else if (charNumbers === 'no') {charNumbers = null}
   else {
     window.alert("Please choose yes or no!");
-    getCharTypes();
+    return getCharTypes();
   }
 
 //special characters
   let charSpecial = window.prompt("Would you like your password to contain special characters? Please type yes or no.").toLowerCase();
   if (charSpecial === "yes") {charSpecial = specialChars}
-  else if (charSpecial === "no") {charSpecial = ""}
+  else if (charSpecial === "no") {charSpecial = null}
   else {
     window.alert("Please choose yes or no!");
-    getCharTypes();
+    return getCharTypes();
   }
 
 //takes array of all character types and converts them to one string
-  let finalPasswordChars = [charLowerCase, charUpperCase, charSpecial, charNumbers].join("")
+  let passwordChars = [charLowerCase, charUpperCase, charSpecial, charNumbers].join("")
 
 
 //return to beginning of function if no valid selection are made.
-    if (!finalPasswordChars) { 
+    if (!passwordChars) { 
       window.alert("You must select at least one type of character for your password! Please try again!");
-      getCharTypes();
+      return getCharTypes();
     } else { 
       window.alert("Conditions accepted! Preparing to generate password now!")
     
     }
-  return finalPasswordChars;
+  return passwordChars;
 
     
 }
 
 
-finalPasswordChars = getCharTypes();
+passwordChars = getCharTypes();
 
 
 //reset password before new one is generated
   password = "";
 
   //add character at random index in finalPasswordChars.length
-  for (i=0; i<passwordLength.length; i++) {
-    let c = Math.floor(Math.random() * finalPasswordChars.length);
-    password += finalPasswordChars.charAt(c)
+  for (i=0; i<newPasswordLength.length; i++) {
+    let c = Math.floor(Math.random() * passwordChars.length);
+    password += passwordChars.charAt(c)
 
   }
   //Randomized Password
